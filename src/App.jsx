77fb329b7b263bdf609df815a1481adaf689db12
Alvaro Nieto <60838509+Anieto86@users.noreply.github.com>
2023-03-24 11:56:45 +1030

    Mouse follower component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { range } from 'd3';
 import React from 'react';
 import './App.css';
-import { CssNamedColors, SmileyFace } from './components';
+import { CssNamedColors, MouseFollower, SmileyFace } from './components';
 
 const array = range(5 * 3);
 
 function App() {
   return (
     <div className='App'>
+      <MouseFollower />
       <CssNamedColors />
       {array.map((ele, i) => {
         return (
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export * from './BackgroundCircle/BackgroundCircle';
 export { default as CssNamedColors } from './CssNamedColors/CssNamedColors';
 export * from './Eyes/Eyes';
+export { default as MouseFollower } from './MouseFollower/MouseFollower';
 export * from './Mouth';
 export { default as SmileyFace } from './SmileyFace/SmileyFace';
 export { default as SmileyFaceTweak } from './SmileyFaceTweak/SmileyFaceTweak';
